384d83c81f56e1099f178c0927a735cf69cf4cb5
Arun-Jawlia <arunkumar08.mk@gmail.com>
2023-10-05 14:49:02 +0530

    Install cra, build folder structure, and write root variable and common css

---
+++ src/components/Homepage/Homepage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div className="header">
+      <div className="header-left">
+        <div className="logo">
+          <img src="../assets/asset 0" alt="" />
+          <img src="" alt="" />
+        </div>
+      </div>
+      <div className="header-right"></div>
+    </div>
+  );
+};
+
+export default Homepage;
